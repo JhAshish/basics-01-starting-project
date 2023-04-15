298f5013c83c2ba9be0c98a7381dc1d3e12401c3
Ashish Jha <ashishjha432@gmail.com>
2023-04-15 15:47:51 +0530

    Show expression on page
    Expression used to evaluate result shown

--- assets/scripts/app.js
+++ assets/scripts/app.js
@@ -1,3 +1,7 @@
+const defulatResult = 0;
 let currentResult = 0;
-currentResult = currentResult + 10;
+
+currentResult = (currentResult + 10) * 3 / 2 + 1;
+
+let calculationDescription = '(' + defulatResult + '+ 10) * 3 / 2 + 1';
 outputResult(currentResult, '');
